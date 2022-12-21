48d00cdf887e3501caf167cb8234770d065449d4
Werner Krauß <werner.krauss@netwerkstatt.at>
2022-12-21 16:37:42 +0100

    PSR-4: move classes to seperate files

---
+++ src/pages/ExplanationPageController.php
@@ -0,0 +1,30 @@
+<?php
+
+class ExplanationPageController extends AbbreviationPageController
+{
+    private static $url_handlers = array(
+        '$Item!' => 'viewItem'
+    );
+    private static $allowed_actions = array(
+        'viewItem'
+    );
+
+    public function viewItem()
+    {
+        $item = $this->getItem($this->request->param('Item'));
+        if (!$item) {
+            $this->httpError(404);
+        }
+        return $this->customise(array('Item' => $item))->renderWith(array('ExplanationPage_view', 'Page'));
+    }
+
+    public function getItem($slug)
+    {
+        return Explanation::get()->filter(array('URLSlug' => $slug))->first();
+    }
+
+    public function getItems()
+    {
+        return Explanation::get()->sort('Title');
+    }
+}
